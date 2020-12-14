7f1e7203d3ec8d87d677a929f151f3b46b132287
Stech97 <maksstech97@gmail.com>
2020-12-14 21:16:37 +0300

    Add design

--- LabWork№1/Form1.cs
+++ LabWork№1/Form1.cs
@@ -1,11 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LabWork_1
@@ -15,6 +7,16 @@ namespace LabWork_1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+
         }
     }
 }
