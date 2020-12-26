3874c857154badf9e9036175e01422b6ae501a27
Stech97 <maksstech97@gmail.com>
2020-12-26 15:06:14 +0300

    Add labwork6
    для девушки

---
+++ LabWork№6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WorkLab_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Объявление счётчика для среднего арефметического
+            int count = 0;
+            // Объявление переменны для ввода 
+            double N = 0;
+            double Total = 0;
+            do
+            {
+                Console.Write("Введите число ");
+                N = double.Parse(Console.ReadLine());
+                Total += N;
+                count++;
+            } while (N >= 3);
+
+            double result = Total / count;
+
+            Console.WriteLine($"Среднее арифметическое введенных чисел:{result}\nКоличество введеных чисел:{count}");
+            Console.ReadLine();
+        }
+    }
+}
